5409671d1ab85105008c3a3837636473f8b1cf52
Venkat R <95846720+venkat0086@users.noreply.github.com>
2022-07-07 12:09:50 +0530

    Updated

--- Week_01/Day_03/nextjs/pages/api/hello.ts
+++
@@ -1,6 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type {} from "next";
-
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
-}
